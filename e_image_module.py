c75510851ab987f4f959666ec7c8d3ac91ecb079
lynn2610 <lynn_mg_study@163.com>
2018-12-11 21:15:14 +0800

    ad e-image-module

---
+++ e_image_module.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/11 19:13
+# @Author  : Lynn
+# @Site    : 
+# @File    : e_image_module.py
+# @Software: PyCharm
+
+import cv2
+from items import STACK
+from items import Tangram
+from RecognitionRotate import ColorContourRecognition, ShapeRecognition
+
+def clear_stack():
+    while STACK.is_empty() == False:
+        STACK.pop()
+
+def get_list(e_image):
+    info = []
+    for i in range(0, 7):
+        if i == 0:
+            color = 'pink'
+            shape = 'triangle'
+        elif i == 6:
+            color = 'red'
+            shape = 'triangle'
+        elif i == 2:
+            color = 'orange'
+            shape = 'triangle'
+        elif i == 3:
+            color = 'yellow'
+            shape = 'parallelogram'
+        elif i == 4:
+            color = 'green'
+            shape = 'triangle'
+        elif i == 5:
+            color = 'blue'
+            shape = 'square'
+        else:
+            color = 'purple'
+            shape = 'triangle'
+
+        mould = ShapeRecognition(1, e_image)
+        mould.completeRecognition(color, shape)
+        (y, x) = (mould.centerP.x, mould.centerP.y)
+        # print(color, shape, x, y)
+
+        info.append(Tangram(color, shape, x, y))
+        # print(len(info))
+    return info
+
+def set_stack(e_image):
+    #清空栈
+    clear_stack()
+    info = get_list(e_image)
+    #根据坐标对每块七巧板进行排序
+    info.sort(key=lambda item:(item.pos_y, item.pos_x))
+    for item in info:
+        STACK.push(item)
+
+
+
+
+if __name__ == '__main__':
+    e_image = cv2.imread('images/mould/5.jpg')
+    # cv2.imshow('image', e_image)
+    # cv2.waitKey(0)
+    # tang = get_list(e_image)
+    # print('printing...')
+    # print(type(tang))
+    # tang.sort(key=lambda item:(item.pos_y, item.pos_x))
+    # for i, item in enumerate(tang):
+    #     print('{0}, {1}, ({2}, {3})'.format(item.color, item.shape, item.pos_x, item.pos_y))
+    set_stack(e_image)
+    while STACK.is_empty() == False:
+        top = STACK.pop()
+        print('{0}, {1}, ({2}, {3})'.format(top.color, top.shape, top.pos_x, top.pos_y))
+
